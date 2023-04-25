5a4cfc105723a010ae3b15512a43f08079547962
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-25 16:55:55 +0100

    Updated hero

--- src/pages/index/Body.tsx
+++ src/pages/index/Body.tsx
@@ -9,12 +9,12 @@ export default function Body() {
       id="body-container"
       className=" grid w-screen  flex-grow pt-2  sm:justify-items-center  "
     >
+      <Hero />
       <section
         id="main-content"
         className="flex h-full w-body min-w-body max-w-body flex-col "
       >
         <div className=" mt-20 w-body-sm min-w-body max-w-body grow sm:w-body">
-          <Hero />
           <ContrastToolCard />
           <DevTipsCard />
           <ColourmaticCard />
