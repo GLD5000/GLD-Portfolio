8cf20b1bf8169e27378597ae705aa32122c12c42
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-26 22:14:19 +0100

    Removed stars

--- src/pages/index/hero/Hero.tsx
+++ src/pages/index/hero/Hero.tsx
@@ -1,10 +1,10 @@
 import GldSvgHero from "@/icons/GldSvgHero";
-import Stars from "./Stars";
+// import Stars from "./Stars";
 
 export default function Hero() {
   return (
     <div className=" absolute top-0  z-[997] grid min-h-screen w-screen  bg-gradient-to-b from-bg-dk via-[98.5%]  to-transparent ">
-      <Stars />
+      {/* <Stars /> */}
       <div className=" h-full w-full bg-transparent">
         <div className="zoom-in mx-auto grid h-fit w-fit gap-12 [transition-duration:1500ms] [transition-delay:100ms]">
           <div className="  mx-auto h-auto w-[98vmin]  drop-shadow-hero dark:drop-shadow-hero-dk sm:w-[80vmin]">
@@ -12,11 +12,11 @@ export default function Hero() {
           </div>
           <div className="zoom-in-slow mx-auto  grid h-fit  w-fit bg-transparent drop-shadow-hero dark:drop-shadow-hero-dk">
             <div className="h-4 w-full rounded-b-full border-8 border-transparent border-x-white border-b-white bg-transparent"></div>
-            <div className="mx-20 grid w-fit grid-cols-2 gap-2 overflow-clip rounded-b-3xl bg-white px-2 pb-2 text-lg font-bold">
-              <button className="h-24 w-full rounded-none rounded-bl-3xl bg-pink-400 p-4 text-black">
+            <div className="mx-4 grid w-fit grid-cols-2 gap-2 overflow-clip rounded-b-3xl bg-white px-2 pb-2 text-lg font-bold">
+              <button className="h-fit w-full rounded-r rounded-br-md rounded-bl-3xl  bg-pink-400 p-4 text-black">
                 About
               </button>
-              <button className="h-24 w-full rounded-none rounded-br-3xl bg-pink-400 p-4 text-black">
+              <button className="h-fit w-full rounded-l rounded-bl-md rounded-br-3xl bg-pink-400 p-4 text-black">
                 Projects
               </button>
             </div>
