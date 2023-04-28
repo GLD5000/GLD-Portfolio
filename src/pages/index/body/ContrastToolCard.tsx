6aa3331db5dbd697fc6329303a3c3bb6b8994419
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-28 13:39:41 +0100

    Added product images

--- src/pages/index/body/ContrastToolCard.tsx
+++ src/pages/index/body/ContrastToolCard.tsx
@@ -1,25 +1,67 @@
-export default function ContrastToolCard() {
-  return (
-    <div className="mx-auto grid w-screen max-w-body items-center self-center px-4 py-10 sm:w-full sm:px-8">
-      <a
-        className=" mx-auto mt-10"
-        href="https://contrast-tool.vercel.app/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="  flex flex-row overflow-clip rounded-lg border-2 border-current ">
-          <h1 className=" rounded-none bg-white p-3 font-black  leading-6 text-black sm:p-[0.95rem] sm:text-6xl">
-            CONTRAST
-          </h1>
-          <h1 className="  rounded-none bg-black p-3 font-black leading-6 text-white sm:p-[0.95rem] sm:text-6xl">
-            LAB
-          </h1>
-        </div>
-      </a>
+import Image from "next/image";
+import ComboCropped from "src/assets/contrasttool/ComboCropped.jpg";
+import InfoTableCropped from "src/assets/contrasttool/InfoTableCropped.jpg";
+import MatrixACropped from "src/assets/contrasttool/MatrixACropped.jpg";
+import MatrixBCropped from "src/assets/contrasttool/MatrixBCropped.jpg";
+import PickerCropped from "src/assets/contrasttool/PickerSoloCropped.jpg";
+import InputCropped from "src/assets/contrasttool/InputCropped.jpg";
 
-      <b className="mx-auto mt-2 mb-20 text-center text-lg sm:text-xl">
-        Create Accessible Colour Combinations
-      </b>
+export default function ColourTheoryCard() {
+  return (
+    <div className="mx-auto grid w-screen  max-w-body items-center self-center px-4 py-10 sm:w-full sm:px-8 lg:grid-cols-2">
+      <div className="grid">
+        <a
+          className=" mx-auto mt-10"
+          href="https://contrast-tool.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="  flex flex-row overflow-clip rounded-lg border-2 border-current ">
+            <h1 className=" rounded-none bg-white p-3 font-black  leading-6 text-black sm:p-[0.95rem] sm:text-6xl">
+              CONTRAST
+            </h1>
+            <h1 className="  rounded-none bg-black p-3 font-black leading-6 text-white sm:p-[0.95rem] sm:text-6xl">
+              LAB
+            </h1>
+          </div>
+        </a>
+
+        <b className="mx-auto mt-2 mb-20 text-center text-lg sm:text-xl">
+          Create Accessible Colour Combinations
+        </b>
+      </div>
+      <div className="relative mx-auto flex aspect-[1/1] w-[80%] max-w-[35rem]">
+        <Image
+          className="absolute bottom-0 left-0 h-[25%] w-auto rounded-md shadow-xl shadow-black"
+          src={PickerCropped}
+          alt="Colour Picker"
+        />
+        <Image
+          className="absolute top-0 left-0 h-[37%] w-auto rounded-md shadow-xl shadow-black"
+          src={InputCropped}
+          alt="Colour Picker"
+        />
+        <Image
+          className="absolute top-[36%] left-0 h-[38%] w-auto rounded-md shadow-xl shadow-black"
+          src={InfoTableCropped}
+          alt="Colour Picker"
+        />
+        {/* <Image
+          className="absolute rounded-md bottom-0 left-0 h-[45%] w-auto shadow-xl shadow-black"
+          src={MatrixBCropped}
+          alt="Colour Picker"
+        /> */}
+        <Image
+          className="absolute bottom-0 right-0 h-[45%] w-auto rounded-3xl shadow-xl shadow-black"
+          src={MatrixACropped}
+          alt="Colour Picker"
+        />
+        <Image
+          className="absolute top-[7%] right-0 h-[53%] w-auto rounded-3xl shadow-xl shadow-black"
+          src={ComboCropped}
+          alt="Colour Picker"
+        />
+      </div>
     </div>
   );
 }
--- src/pages/index/body/DevTipsCard.tsx
+++ src/pages/index/body/DevTipsCard.tsx
@@ -1,21 +1,69 @@
-export default function DevTipsCard() {
+import Image from "next/image";
+import BlankSectionCropped from "src/assets/devtips/BlankSectionCropped.jpg";
+import CreateCropped from "src/assets/devtips/CreateCropped.jpg";
+import SectionCropped from "src/assets/devtips/SectionCropped.jpg";
+import tagsCropped from "src/assets/devtips/tagsCropped.jpg";
+import TipBCropped from "src/assets/devtips/TipBCropped.jpg";
+import TipCropped from "src/assets/devtips/TipCropped.jpg";
+import TipsCropped from "src/assets/devtips/TipsCroppedB.jpg";
+
+export default function ColourTheoryCard() {
   return (
-    <div className="mx-auto grid w-screen max-w-body items-center self-center px-4 py-10 sm:w-full sm:px-8">
-      <a
-        className=" mx-auto mt-10"
-        href="https://glddevtips.web.app/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className=" flex flex-row overflow-clip rounded-lg border-2 border-current ">
-          <h1 className=" rounded-none bg-white px-2 pb-4 pt-1 font-devTips text-6xl font-black tracking-wide text-black sm:px-4 sm:pt-2 sm:pb-8 sm:text-8xl">
-            DevTips
-          </h1>
-        </div>
-      </a>
-      <b className=" mx-auto mt-2 mb-20 text-center text-lg sm:text-xl">
-        Tips and Tricks for Developers
-      </b>
+    <div className="mx-auto grid w-screen  max-w-body items-center self-center px-4 py-10 sm:w-full sm:px-8 lg:grid-cols-2">
+      <div className="grid">
+        <a
+          className=" mx-auto mt-10"
+          href="https://glddevtips.web.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className=" flex flex-row overflow-clip rounded-lg border-2 border-current ">
+            <h1 className=" rounded-none bg-white px-2 pb-4 pt-1 font-devTips text-6xl font-black tracking-wide text-black sm:px-4 sm:pt-2 sm:pb-8 sm:text-8xl">
+              DevTips
+            </h1>
+          </div>
+        </a>
+        <b className=" mx-auto mt-2 mb-20 text-center text-lg sm:text-xl">
+          Tips and Tricks for Developers
+        </b>{" "}
+      </div>
+      <div className="relative mx-auto flex aspect-[1/1] w-[80%] max-w-[35rem] ">
+        {/* <Image
+          className="absolute rounded-md bottom-[45%] right-0 h-[45%] w-auto shadow-xl shadow-black"
+          src={BlankSectionCropped}
+          alt="Colour Picker"
+        /> */}
+        <Image
+          className="absolute top-0 left-0 h-[37%] w-auto rounded-md shadow-xl shadow-black"
+          src={TipsCropped}
+          alt="Colour Picker"
+        />
+        <Image
+          className="absolute top-[31%] right-0 h-[39%] w-auto rounded-md shadow-xl shadow-black"
+          src={tagsCropped}
+          alt="Colour Picker"
+        />
+        {/* <Image
+          className="absolute rounded-md bottom-0 right-0 h-[37%] w-auto shadow-xl shadow-black"
+          src={TipBCropped}
+          alt="Colour Picker"
+        /> */}
+        <Image
+          className="absolute bottom-0 left-0 h-[50%] w-auto rounded-md object-contain shadow-xl shadow-black"
+          src={TipCropped}
+          alt="Colour Picker"
+        />
+        <Image
+          className="absolute bottom-[4%] right-0 h-[41%] w-auto rounded-md shadow-xl shadow-black"
+          src={SectionCropped}
+          alt="Colour Picker"
+        />
+        <Image
+          className="absolute bottom-[80%] right-0 h-[15%] w-auto rounded-md shadow-xl shadow-black"
+          src={CreateCropped}
+          alt="Colour Picker"
+        />
+      </div>
     </div>
   );
 }
