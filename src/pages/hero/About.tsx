3ad07a0655b39af771322455f528d598406844d8
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-04 13:39:49 +0100

    Added stack modal

--- src/pages/hero/About.tsx
+++ src/pages/hero/About.tsx
@@ -1,4 +1,8 @@
+import { useRef } from "react";
+import Link from "next/link";
+
 export default function About() {
+  const stackModal = useRef<HTMLDialogElement>(null);
   return (
     <section
       id="about"
@@ -18,17 +22,73 @@ export default function About() {
                 From software, to music, sound and graphics: I love to create
                 and believe in the power of innovation. I enjoy finding new ways
                 to tackle challenges within a project and create value for
-                users. These are the tools I use to make it happen.
+                users. Click to see the tools I use to make it happen.
               </p>
             </div>
-            <ul className="list-disc">
-              <li>React</li>
-              <li>Next.js</li>
-              <li>Tailwind</li>
-              <li>Vite</li>
-              <li>Vitest</li>
-              <li>Husky</li>
-            </ul>
+            <button
+              type="button"
+              className="m-auto h-fit w-full bg-brand p-4 text-xl"
+              onClick={() => {
+                stackModal.current?.showModal();
+              }}
+            >
+              My Stack
+            </button>
+            <dialog
+              className="m-auto gap-6 rounded p-10 text-black lg:grid-cols-3"
+              ref={stackModal}
+            >
+              <figure>
+                <figcaption className="text-bold mb-4 text-center text-xl">
+                  My Stack
+                </figcaption>
+                <ul className="list-disc">
+                  <li>React</li>
+                  <li>Next.js</li>
+                  <li>Tailwind</li>
+                  <li>Vite</li>
+                  <li>Vitest</li>
+                  <li>Husky</li>
+                </ul>
+              </figure>
+
+              <figure>
+                <figcaption className="text-bold mb-4 text-center text-xl">
+                  Web Skills
+                </figcaption>
+                <ul className="list-disc">
+                  <li>TypeScript</li>
+                  <li>JavaScript</li>
+                  <li>Tailwind CSS</li>
+                  <li>Sassy CSS</li>
+                  <li>HTML 5</li>
+                </ul>
+              </figure>
+
+              <figure>
+                <figcaption className="text-bold mb-4 text-center text-xl">
+                  Other Skills
+                </figcaption>
+                <ul className="list-disc">
+                  <li>
+                    <Link
+                      href="https://www.cockos.com/EEL2/"
+                      target="_blank"
+                      referrerPolicy="no-referrer"
+                    >
+                      EEL2
+                    </Link>{" "}
+                    Software Plugin Development
+                  </li>
+                  <li>Lua Scripting</li>
+                  <li>Visual Basic for Application (VBA)</li>
+                  <li>Google Apps Script (GAS)</li>
+                  <li>Vector-Based Graphic Design - InkScape</li>
+                  <li>Sound Design</li>
+                  <li>Music Production - Reaper / Cubase</li>
+                </ul>
+              </figure>
+            </dialog>
           </div>
           <div className="grid gap-10 md:grid-cols-2">
             <div>
