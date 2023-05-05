014b3ff82c8457663b8ceb62a3194276374a2e48
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-05 20:39:40 +0100

    Added utilities

---
+++ src/utilities/contexts/IntersectionProvider.tsx
@@ -0,0 +1,47 @@
+import {
+  createContext,
+  type ReactNode,
+  useContext,
+  type Dispatch,
+  useState,
+  type SetStateAction,
+} from "react";
+
+export interface ProviderState {
+  currentSection: string;
+  hasScrolled: boolean;
+}
+
+export interface ProviderContext extends ProviderState {
+  setcurrentSection: Dispatch<SetStateAction<string>>;
+  setHasScrolled: Dispatch<SetStateAction<boolean>>;
+}
+
+const initialiserContext: ProviderContext = {
+  currentSection: "hero-section",
+  hasScrolled: false,
+  setcurrentSection: () => undefined,
+  setHasScrolled: () => undefined,
+};
+
+function useData() {
+  const [currentSection, setcurrentSection] = useState("hero-section");
+  const [hasScrolled, setHasScrolled] = useState(false);
+  return {
+    currentSection,
+    setcurrentSection,
+    hasScrolled,
+    setHasScrolled,
+  };
+}
+
+const Intersection = createContext(initialiserContext);
+export const useIntersectionProviderContext = () => useContext(Intersection);
+export default function IntersectionProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const data = useData();
+  return <Intersection.Provider value={data}>{children}</Intersection.Provider>;
+}
---
+++ src/utilities/hooks/useIntersectionObserver.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useIntersectionObserver(
+  options: Partial<{
+    root: null | Element;
+    rootMargin: string;
+    threshold: number | number[];
+  }> = {
+    root: null,
+    rootMargin: "-20% 0px -50% 0px",
+    threshold: [0.1, 0.15, 0.2],
+  }
+) {
+  const elementRef = useRef<HTMLDivElement>(null);
+  const [onScreen, setOnScreen] = useState(false);
+
+  function callback(entries: IntersectionObserverEntry[]) {
+    const [entry] = entries;
+    setOnScreen(entry.isIntersecting);
+  }
+
+  useEffect(() => {
+    let run = true;
+    const { current } = elementRef;
+    const observer = new IntersectionObserver(callback, options);
+    if (run) {
+      if (current !== null) observer.observe(current);
+    }
+    return () => {
+      run = false;
+      if (current !== null) observer.unobserve(current);
+    };
+  }, [elementRef, onScreen, options]);
+
+  return { elementRef, onScreen };
+}
