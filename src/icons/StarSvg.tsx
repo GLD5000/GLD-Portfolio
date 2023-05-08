ecc293bfe7b176d62a67e2cc28967804bcf72c1f
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-08 13:42:41 +0100

    Added cute star

---
+++ src/icons/StarSvg.tsx
@@ -0,0 +1,73 @@
+export default function StarSvg({
+  classes = "stroke-current fill-current stroke-2 [transition-duration:800ms] [transition-delay:100ms] hover:fill-brand ",
+}) {
+  return (
+    <div className="h-full w-full">
+      <svg
+        style={{
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+        }}
+        id="add-svg"
+        role="img"
+        aria-label="Add Section"
+        height="100%"
+        width="100%"
+        viewBox="0 0 48 48"
+      >
+        <path
+          transform="rotate(4.2164765,-155.86817,176.03988)"
+          d="M 25.910021,31.790519 12.159582,23.724193 -1.5724123,32.035806 1.8500001,16.465729 -10.298233,5.9742563 5.5673735,4.4177456 11.791346,-10.377954 18.174418,4.2301461 34.169278,5.5773734 22.248627,16.162187 Z"
+          className={classes}
+        />
+      </svg>
+    </div>
+  );
+}
+
+// <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+// <!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+// <svg
+//    version="1.1"
+//    id="svg970"
+//    xml:space="preserve"
+//    width="46.376053"
+//    height="45.092148"
+//    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+//    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+//    xmlns="http://www.w3.org/2000/svg"
+//    xmlns:svg="http://www.w3.org/2000/svg"><defs
+//      id="defs974" /><sodipodi:namedview
+//      id="namedview972"
+//      pagecolor="#505050"
+//      bordercolor="#ffffff"
+//      borderopacity="1"
+//      inkscape:showpageshadow="0"
+//      inkscape:pageopacity="0"
+//      inkscape:pagecheckerboard="1"
+//      inkscape:deskcolor="#505050" /><inkscape:clipboard
+//      style="font-variation-settings:normal;opacity:1;vector-effect:none;fill:#ffffff;fill-opacity:1;stroke:#ffffff;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;-inkscape-stroke:none;stop-color:#000000;stop-opacity:1"
+//      min="0.81197255,1.4539266"
+//      max="47.188026,46.546073"
+//      geom-min="1.811837,2.4537735"
+//      geom-max="46.188172,45.546187" /><g
+//      id="g976"
+//      transform="translate(-0.81197255,-1.4539266)"><path
+//        sodipodi:type="star"
+//        style="fill:#ffffff;fill-opacity:1;stroke:#ffffff;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+//        id="path786"
+//        inkscape:flatsided="false"
+//        sodipodi:sides="5"
+//        sodipodi:cx="12"
+//        sodipodi:cy="13"
+//        sodipodi:r1="23.378885"
+//        sodipodi:r2="10.72538"
+//        sodipodi:arg1="0.9335528"
+//        sodipodi:arg2="1.5559169"
+//        inkscape:rounded="0"
+//        inkscape:randomized="0"
+//        d="M 25.910021,31.790519 12.159582,23.724193 -1.5724123,32.035806 1.8500001,16.465729 -10.298233,5.9742563 5.5673735,4.4177456 11.791346,-10.377954 18.174418,4.2301461 34.169278,5.5773734 22.248627,16.162187 Z"
+//        inkscape:transform-center-x="-0.53210316"
+//        inkscape:transform-center-y="-1.2084291"
+//        transform="rotate(4.2164765,-155.86817,176.03988)" /></g></svg>
--- src/pages/index/hero/Hero.tsx
+++ src/pages/index/hero/Hero.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useIntersectionProviderContext } from "@/utilities/contexts/IntersectionProvider";
 import useIntersectionObserver from "@/utilities/hooks/useIntersectionObserver";
 import SpanSplitter from "./SpanSplitter";
+import StarSvg from "@/icons/StarSvg";
 
 export default function Hero() {
   const { elementRef, onScreen } = useIntersectionObserver();
@@ -34,44 +35,49 @@ export default function Hero() {
         ref={elementRef}
         className=" absolute top-0  z-[997] grid h-screen w-full "
       >
-        <div className=" mx-auto grid h-full w-full items-center gap-8 lg:grid-cols-2 2xl:w-fit">
-          <div className="mx-auto h-full w-screen max-w-[35rem] drop-shadow-hero-dk  lg:w-full lg:max-w-[50rem] ">
+        <div className=" mx-auto grid h-full w-min items-center gap-8 lg:w-fit lg:grid-cols-2">
+          <div className="mx-auto h-full w-screen max-w-[35rem] drop-shadow-hero-dk lg:h-auto lg:w-full lg:max-w-[50rem] ">
             <GldSvgHero />
           </div>
-          <div className="grid h-fit gap-2">
-            <p className=" mx-auto mb-0 w-fit text-xl lg:text-2xl xl:text-3xl">
+          <div className="mx-auto grid h-fit w-max gap-2">
+            {/* <p className=" ml:4 mr-auto mb-0 w-fit text-xl md:ml-16 lg:text-2xl  xl:text-3xl">
               <SpanSplitter word={"Hi, I am"} />
-            </p>
+            </p> */}
 
-            <h1 className="mx-auto flex flex-row text-4xl font-bold xs:text-5xl xl:text-7xl 2xl:text-8xl">
-              <span
-                key="g"
-                className="colour-in-g cursor-default [transition-duration:800ms] [transition-delay:100ms] hover:text-brand"
-              >
-                Gareth{" "}
-              </span>
-              <span
-                key="l"
-                className="colour-in-l cursor-default [transition-duration:800ms] [transition-delay:100ms] hover:text-brand"
-              >
-                L{" "}
-              </span>
-              <span
-                key="d"
-                className="colour-in-d cursor-default [transition-duration:800ms] [transition-delay:100ms] hover:text-brand"
-              >
-                Devlin
-              </span>
-            </h1>
+            <div className="px:4 relative mx-auto w-fit max-w-[90%] pt-4 pb-4 md:px-16  md:pt-10">
+              <h1 className="mx-auto grid grid-cols-frAutoFr gap-2 text-3xl font-bold xs:text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl">
+                <span
+                  key="g"
+                  className="colour-in-g cursor-default [transition-duration:800ms] [transition-delay:100ms] hover:text-brand"
+                >
+                  Gareth{" "}
+                </span>
+                <span
+                  key="l"
+                  className="colour-in-l cursor-default [transition-duration:800ms] [transition-delay:100ms] hover:text-brand"
+                >
+                  L{" "}
+                </span>
+                <span
+                  key="d"
+                  className="colour-in-d cursor-default [transition-duration:800ms] [transition-delay:100ms] hover:text-brand"
+                >
+                  Devlin
+                </span>
+              </h1>
+              <div className=" wobblingStar absolute right-0 top-0 aspect-square h-6  sm:h-8 md:h-10 lg:h-12 xl:h-14">
+                <StarSvg />
+              </div>
+            </div>
             <h2 className="mx-auto text-2xl font-bold xl:text-4xl">
               <SpanSplitter word={"Web Developer"} />
             </h2>
-            <p className="my-0 mx-auto w-fit text-sm sm:text-base md:text-lg lg:text-xl">
+            <p className="my-0 mx-auto w-fit text-sm sm:text-base lg:text-lg xl:text-xl">
               <SpanSplitter
                 word={"I am a React developer based near London, UK."}
               />
             </p>
-            <p className="my-0 mx-auto w-fit text-sm sm:text-base md:text-lg lg:text-xl">
+            <p className="my-0 mx-auto w-fit text-sm sm:text-base lg:text-lg xl:text-xl">
               <SpanSplitter
                 word={"I build innovative and accessible websites and apps."}
               />
