f35c0d37157d62bc723ea521fdf056f208120739
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-09 23:27:08 +0100

    Added links and images

--- src/elements/Modal.tsx
+++ src/elements/Modal.tsx
@@ -14,14 +14,14 @@ export default function Modal({
   return (
     <dialog
       ref={refIn}
-      className="m-auto max-w-[95vw] gap-6 rounded-3xl border-2 bg-purple-light p-10 text-black dark:bg-purple dark:text-white"
+      className="m-auto max-w-[95vw] gap-6 rounded-3xl border-2 bg-purple-light p-2 text-black dark:bg-purple dark:text-white"
     >
       <button
         id="close-btn"
         aria-label="close-information-box"
         type="button"
         onClick={handleClose}
-        className="bg-palegrey absolute top-2 right-2 h-8 w-8 rounded-full border-2 bg-purple p-1 text-white hover:bg-white hover:text-black hover:transition focus:bg-white focus:text-black focus:transition"
+        className="bg-palegrey sticky top-0 left-[100%] mr-auto h-8 w-8 rounded-full border-2 bg-purple p-1 text-white hover:bg-white hover:text-black hover:transition focus:bg-white focus:text-black focus:transition"
       >
         <CloseSvg />
       </button>
--- src/pages/index/about/modals/CreativityModal.tsx
+++ src/pages/index/about/modals/CreativityModal.tsx
@@ -4,7 +4,11 @@ import { RefObject } from "react";
 import Image from "next/image";
 import scope from "src/assets/aboutme/ScopeCropped.jpg";
 import scales from "src/assets/aboutme/ScalesCroppedB.jpg";
-
+import pool from "src/assets/aboutme/poolPodcastCropped.jpg";
+import morethan from "src/assets/aboutme/morethanPodcast.jpg";
+import assessment from "src/assets/aboutme/assessmentPortal.jpg";
+import cert from "src/assets/aboutme/cert.jpg";
+import logo from "src/assets/aboutme/gldInkscape.jpg";
 export default function CreativityModal({
   refPassed,
 }: {
@@ -14,72 +18,268 @@ export default function CreativityModal({
     <Modal
       refIn={refPassed}
       content={
-        <div className="grid gap-6 p-4 lg:grid-cols-3">
-          <figure>
-            <figcaption className="text-bold mb-4 text-center text-xl">
-              My Stack
-            </figcaption>
-            <ul className="list-disc pl-6">
-              <li>React</li>
-              <li>Next.js</li>
-              <li>Tailwind</li>
-              <li>Vite</li>
-              <li>Vitest</li>
-              <li>Husky</li>
-              <li>Lint-Staged</li>
-              <li>VSCode</li>
-              <li>GitHub</li>
-            </ul>
-          </figure>
-          <figure>
-            <figcaption className="text-bold mb-4 text-center text-xl">
-              Web Skills
-            </figcaption>
-            <ul className="list-disc pl-6">
-              <li>React</li>
-              <li>TypeScript</li>
-              <li>JavaScript</li>
-              <li>Tailwind CSS</li>
-              <li>SCSS</li>
-              <li>HTML5</li>
-            </ul>
-          </figure>
-          <figure>
-            <figcaption className="text-bold mb-4 text-center text-xl">
+        <div className="grid gap-10 p-4 ">
+          <div className="grid max-w-[30rem] px-8">
+            <h2 className="mx-auto mb-4 w-fit text-center text-2xl font-bold">
+              Web Skills / My Stack
+            </h2>
+            <div className="flex flex-wrap gap-6">
+              <Link
+                href="https://react.dev/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                React
+              </Link>
+              <Link
+                href="https://www.typescriptlang.org/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                TypeScript
+              </Link>
+              <Link
+                href="https://nextjs.org"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Next.js
+              </Link>
+              <Link
+                href="https://tailwindcss.com/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Tailwind CSS
+              </Link>
+              <Link
+                href="https://vitejs.dev/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Vite
+              </Link>
+              <Link
+                href="https://vitest.dev/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Vitest
+              </Link>
+              <Link
+                href="https://www.npmjs.com/package/husky"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Husky
+              </Link>
+              <Link
+                href="https://www.npmjs.com/package/lint-staged"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Lint-Staged
+              </Link>
+              <Link
+                href="https://code.visualstudio.com/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                VSCode
+              </Link>
+              <Link
+                href="https://github.com/GLD5000"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                GitHub
+              </Link>
+              <Link
+                href="https://git-scm.com/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                Git
+              </Link>
+              <Link
+                href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                JavaScript
+              </Link>
+              <Link
+                href="https://sass-lang.com/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                SCSS
+              </Link>
+              <Link
+                href="https://html.spec.whatwg.org/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                {" "}
+                HTML5+
+              </Link>
+              <Link
+                href={
+                  "https://freecodecamp.org/certification/GLD5000/javascript-algorithms-and-data-structures"
+                }
+                target="_blank"
+                referrerPolicy="no-referrer"
+                aria-label="Go to my Free code camp certificate"
+              >
+                <Image
+                  placeholder="blur"
+                  src={cert}
+                  className="m-auto h-auto w-full rounded shadow-xl shadow-black"
+                  alt={"Freecode camp certificate"}
+                />
+              </Link>
+            </div>
+          </div>
+
+          <div className="mx-auto grid max-w-[30rem] items-center gap-6 px-8">
+            <h2 className="mx-auto mb-4 w-fit text-center text-2xl font-bold">
               Other Skills
-            </figcaption>
-            <ul className="list-disc pl-6">
-              <li>
-                <Link
-                  href="https://www.cockos.com/EEL2/"
-                  target="_blank"
-                  referrerPolicy="no-referrer"
-                  className="underline"
-                >
-                  EEL2
-                </Link>{" "}
+            </h2>
+            <div className="grid items-center gap-2">
+              <h3 className="mx-auto flex h-fit w-fit gap-6 text-center">
                 Music Plugin Development
-              </li>
-              <li>Lua Scripting</li>
-              <li>Visual Basic for Application (VBA)</li>
-              <li>Google Apps Script (GAS)</li>
-              <li>Graphic Design - InkScape</li>
-              <li>Music Production - Reaper / Cubase</li>
-              <li>Sound Design</li>
-            </ul>
-          </figure>
-          <Image
-            placeholder="blur"
-            src={scope}
-            className="m-auto h-auto w-full rounded shadow-xl shadow-black"
-            alt={"GLD Scope Oscilloscope"}
-          />
-          <Image
-            placeholder="blur"
-            src={scales}
-            className="m-auto h-auto w-full rounded-3xl shadow-xl shadow-black"
-            alt={"GLD Scope Oscilloscope"}
-          />
+              </h3>
+              <Link
+                href="https://www.cockos.com/EEL2/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                EEL2
+              </Link>
+
+              <Image
+                placeholder="blur"
+                src={scope}
+                className="m-auto h-auto w-full rounded shadow-xl shadow-black"
+                alt={"GLD Scope Oscilloscope"}
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <h3 className="mx-auto h-fit w-fit text-center">
+                Google Apps Script
+              </h3>
+              <Link
+                href="https://www.google.com/script/start/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                GAS
+              </Link>
+              <Image
+                placeholder="blur"
+                src={assessment}
+                className="m-auto h-auto w-full rounded-3xl shadow-xl shadow-black"
+                alt={"GLD Scope Oscilloscope"}
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <h3 className="mx-auto h-fit w-fit text-center">
+                Graphic Design
+              </h3>
+              <Link
+                href="https://inkscape.org/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                Inkscape
+              </Link>
+
+              <div className="relative aspect-[2/1.2] w-full">
+                <Image
+                  placeholder="blur"
+                  src={scales}
+                  className="absolute right-0 bottom-0 m-auto h-[60%] w-auto rounded-3xl shadow-xl shadow-black"
+                  alt={"GLD Scope Oscilloscope"}
+                />
+                <Image
+                  placeholder="blur"
+                  src={logo}
+                  className="absolute left-0 top-0 m-auto h-auto w-[45%] rounded-3xl shadow-xl shadow-black"
+                  alt={"GLD Scope Oscilloscope"}
+                />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <h3 className="mx-auto h-fit w-fit text-center">
+                Music Production
+              </h3>
+              <p className="mx-auto mb-0 w-fit text-sm">
+                Including some theme music for podcasts
+              </p>
+              <Link
+                href="https://inkscape.org/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="mx-auto w-fit rounded bg-brand p-1 text-white"
+              >
+                Reaper
+              </Link>
+              <div className="relative aspect-[2/1.2] w-full">
+                <Image
+                  placeholder="blur"
+                  src={morethan}
+                  className="absolute left-0 top-0 m-auto h-auto w-[55%] rounded-3xl shadow-xl shadow-black"
+                  alt={"GLD Scope Oscilloscope"}
+                />
+                <Image
+                  placeholder="blur"
+                  src={pool}
+                  className="absolute right-0 bottom-0 m-auto h-auto w-[55%] rounded-3xl shadow-xl shadow-black"
+                  alt={"GLD Scope Oscilloscope"}
+                />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <h3 className="mx-auto h-fit w-fit text-center">Sound Design</h3>
+              {/* add image */}
+            </div>
+            <h3 className="mx-auto h-fit w-fit text-center">Lua Scripting</h3>
+            <h3 className="mx-auto h-fit w-fit text-center">
+              Visual Basic for Application (VBA)
+            </h3>
+          </div>
         </div>
       }
     />
