fe4c19b4c87378cfe17824bc53a9a5f2400a3b8c
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-10 18:07:29 +0100

    Added performance image

--- src/pages/index/about/modals/AccessibilityModal.tsx
+++ src/pages/index/about/modals/AccessibilityModal.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
 import Modal from "../../../../elements/Modal";
+import Image from "next/image";
 import { RefObject } from "react";
+import scores from "src/assets/aboutme/Performance.jpg";
 
 export default function AccessibilityModal({
   refPassed,
@@ -11,12 +12,12 @@ export default function AccessibilityModal({
     <Modal
       refIn={refPassed}
       content={
-        <div className="grid lg:grid-cols-2">
+        <div className="grid max-w-[30rem] px-8">
           <figure>
             <figcaption className="text-bold mb-4 text-center text-xl">
               GLD Designs Include:
             </figcaption>
-            <ul className=" list-disc pl-6 ">
+            <ul className=" list-none pl-6 ">
               <li>Links that skip to main content for keyboard navigation</li>
               <li>
                 Light and Dark modes for different abilities and preferences
@@ -27,10 +28,17 @@ export default function AccessibilityModal({
           </figure>
 
           <figure>
+            <Image
+              placeholder="blur"
+              src={scores}
+              className="m-auto h-auto w-full rounded shadow-xl shadow-black"
+              alt={"Freecode camp certificate"}
+            />
+
             <figcaption className="text-bold mb-4 text-center text-xl">
               My Tools for Developers:
             </figcaption>
-            <ul className=" list-disc pl-6 ">
+            <ul className=" list-none pl-6 ">
               <li>
                 Colour Scheme generator with automatic text colours & tonal
                 gradients
