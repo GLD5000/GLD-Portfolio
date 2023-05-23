6b72762f3b9f3becd3486ab57bf13e7e5a62c107
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-23 23:24:27 +0100

    Updated position

--- src/pages/homepage/header/MainContentLink.tsx
+++ src/pages/homepage/header/MainContentLink.tsx
@@ -1,7 +1,7 @@
 export default function MainContentLink() {
   return (
     <a
-      className="top:0 text-darkblue absolute left-0 h-fit w-fit focus:top-16 focus:left-16 focus:z-[998] focus:transition dark:text-white"
+      className="text-darkblue absolute top-16 left-16 h-6 w-48 items-center text-center focus:z-[998] focus:transition dark:text-white"
       href="#projects"
     >
       Skip to main content
