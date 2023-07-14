84df39d5eb18f9f720abafb5e355e3d803027650
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-14 13:20:44 +0100

    Swapped image and title

--- src/pages/homepage/contact/Contact.tsx
+++ src/pages/homepage/contact/Contact.tsx
@@ -11,16 +11,16 @@ function Contact() {
     <section className="m-auto grid w-full gap-4 rounded bg-secondary-light pt-20 pb-32  text-black  dark:bg-secondary dark:text-white">
       <div className="m-auto grid w-body-sm  min-w-body max-w-body gap-4 rounded bg-transparent  text-black dark:text-white  sm:w-body">
         <div className="mx-auto mt-8 grid w-fit gap-4 sm:mt-0 sm:grid-cols-2">
+          <div className="m-auto w-fit">
+            <IntersectionReference identity="contact" header="Hi There!" />
+            <p className="m-0 mx-auto text-center text-xl sm:text-4xl">{`Let's chat!`}</p>
+          </div>
           <Image
             placeholder="blur"
             src={portrait}
             className="m-auto h-40 w-40 rounded-full shadow-xl shadow-black"
             alt="Gareth Photo"
           />
-          <div className="m-auto w-fit">
-            <IntersectionReference identity="contact" header="Hi There!" />
-            <p className="m-0 mx-auto text-center text-4xl">{`Let's chat!`}</p>
-          </div>
         </div>
         <ContactForm />
         <p className="m-0 mx-auto text-center text-2xl">
